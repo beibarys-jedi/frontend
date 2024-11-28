782b2bc1452b0538b8015cd038ddc52e01a2ec6a
Beibarys Umirbayev <b.umirbayev@ecc.kz>
2024-11-28 20:56:45 +0500

    add register

---
+++ src/pages/Register.tsx
@@ -0,0 +1,14 @@
+function Register() {
+  return (
+    <div>
+      <h1>welcome to egov page</h1>
+      <p>Порталға кіру</p>
+      <span>ЭЦҚ</span>
+      <div>
+        <button>Сертификат таңдаңыз</button>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
